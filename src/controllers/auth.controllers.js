cc313268d6f8f5b773f0c2c664c853a8e97ac2d6
Damilola Adekoya <dharmykoya38@gmail.com>
2019-06-29 11:03:33 +0100

    feature(validation):fixing validation Fixing the validation methods [#166816100]

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,16 +1,10 @@
 import { validationResult } from 'express-validator';
 import Helper from '../services/helper';
-import  authService  from '../services/auth.services'
-
+import authService from '../services/auth.services';
 
 const signUp = async (request, response) => {
-    const errors = validationResult(request);
-    console.log(errors);
-    if (!errors.isEmpty()) {
-      return Helper.errorResponse(response, 422, errors);
-    }
-	const value =  await authService.authService(request.body);
-	return response.status(201).json(value)
+  const value = await authService.authService(request.body);
+  return response.status(201).json(value);
 };
 
 export default { signUp };
--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -1,9 +1,14 @@
-import express from  'express';
-import authValidator from '../../validators/user.validator'
+import express from 'express';
+import authValidator from '../../validators/user.validator';
 import authController from '../../controllers/auth.controllers';
 
 const router = express.Router();
 
-router.post('/signup', authValidator.signUpValidator, authController.signUp);
+router.post(
+  '/signup',
+  authValidator.signUpValidator('signup'),
+  authValidator.checkValidationResult,
+  authController.signUp
+);
 
 export default router;
