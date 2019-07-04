bef7ec7067f1cd377971fd5007cfcb1132804951
Halimah Oladosu <halimaholadosu@Halimahs-MacBook-Pro.local>
2019-07-04 16:25:33 +0100

    Fix error

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import authentication from '../../middlewares/auth.middleware';
 import userController from '../../controllers/user.controller';
-import tryCatch from '../../helpers/tryCatch.helper';
 
 const router = express.Router();
 
-router.get('/', authentication.verifyToken, tryCatch(userController.getUsers));
+router.get('/', authentication.verifyToken, userController.getUsers);
 
 export default router;
